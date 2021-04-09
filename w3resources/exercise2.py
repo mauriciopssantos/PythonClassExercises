2d374b0cc9a1d5a0bf56751db5426b1e62d7c88b
Mauricio Peregrino <mauriciopssantos1@gmail.com>
2021-04-09 13:19:12 -0300

    Solving exercise2

--- w3resources/exercise2.py
+++ w3resources/exercise2.py
@@ -1,1 +1,14 @@
 #Write a Python program to create a class and display the namespace of the said class
+class NameGenerator:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def __call__(self):
+        print("My name is {}, and I'm {} years old".format(self.name,self.age))
+
+M = NameGenerator("Mauricio","23")
+M()
+
+for attributes in NameGenerator.__dict__:
+    print(attributes)
